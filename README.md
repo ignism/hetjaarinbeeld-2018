f16c0e966e78dfb7d153bcaf01e36110f21a6eb8
ignism <markbrand@Marks-iMac.fritz.box>
2019-03-12 10:17:05 +0100

    clean start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hetjaarinbeeld-2018
 Bibliotheenk Eindhoven jaarverslag 2018
+
+## Setup
+
+`npm install`
+`cd public`
+`composer install`
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new Twig_Environment($loader);
+
+$title = 'Het Jaar In Beeld';
+
+$content = [
+    'title' => $title
+];
+
+echo $twig->render('index.html.twig', $content);
